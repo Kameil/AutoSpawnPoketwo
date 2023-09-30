571f9f3fc06b6a6820df880fc852f75c271c8408
Kameil <130675534+Kameil@users.noreply.github.com>
2023-09-30 14:20:57 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,60 @@
+import discord, random, os, asyncio, keep_alive
+from discord.ext import commands
+
+paused = False
+spam_id = os.environ['spam_id']
+client, token = commands.Bot(command_prefix='!', help_command=None), os.environ['token']
+
+
+async def spam(canal, time):
+    if canal:
+        while True:
+            try:
+                if not paused:
+                    await canal.send(''.join(random.sample(['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'], 7) * 4))
+                await asyncio.sleep(random.choice(time))
+            except:
+                await asyncio.sleep(60)
+                pass
+    else:
+        raise RuntimeError('o canal nao foi encontrado verifique seu spam_id')
+    
+
+@client.event
+async def on_ready():
+    print('Logado em {0}'.format(client.user.name))
+    canal = client.get_channel(int(spam_id))
+    await spam(canal, [3.5, 3.4, 2, 5, 4.3])
+
+@client.event
+async def on_message(message):
+    if not message.author.bot and str(message.channel.id) == str(spam_id):
+        await client.process_commands(message)
+
+
+
+@client.command(name='stop', aliases=['pause'])
+async def stop(ctx):
+    if not str(ctx.channel.id) == str(spam_id):
+        return
+    global paused
+    if not paused:
+        paused = True
+        await ctx.send('Bot pausado')
+    else:
+        await ctx.send('bot ja esta pausado')
+
+@client.command(name='start', aliases=['unpause'])
+async def start(ctx):
+    if not str(ctx.channel.id) == str(spam_id):
+        return
+    global paused
+    if paused:
+        paused = False
+        await ctx.send('bot despausado')
+    else:
+        await ctx.send('bot ja esta em execuçao')
+
+
+keep_alive.keep_alive()
+client.run(token)
